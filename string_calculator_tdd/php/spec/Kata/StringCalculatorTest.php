ae0f52edbe1877c1888864e11ebb0e1972bcc458
Simone Pasquini <simone.pasquini@neomobile.com>
2014-08-05 14:01:21 +0200

    Create Skeleton First test green

---
+++ string_calculator_tdd/php/spec/Kata/StringCalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kata;
+
+class StringCalculatorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testAddingZeroResponseisEmpty()
+    {
+        $obj = new StringCalculator();
+        $response = $obj->add("0");
+        $this->assertEquals("", $response);
+    }
+}
---
+++ string_calculator_tdd/php/src/Kata/StringCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kata;
+
+class StringCalculator {
+
+    public function add($number) {
+        return "";
+    }
+
+}
