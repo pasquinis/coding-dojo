9f2939a40e93777c76cc0a0ba375f328f0f89b9c
Simone Pasquini <simone.pasquini@gmail.com>
2016-01-20 12:22:10 +0000

    Removed duplication caused by introduction of second test

--- bowling_tdd/101/BowlingGameTest.php
+++ bowling_tdd/101/BowlingGameTest.php
@@ -4,12 +4,25 @@ require_once('Game.php');
 
 class BowlingGameTest extends PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->game = new Game();
+    }
+
     public function testTheGameIsAllZero()
     {
-        $game = new Game();
-        for($i = 0; $i < 20; $i++) {
-            $game->roll(0);
+        $this->addSameNumberOfPins(20, 0);
+        $this->assertEquals(0, $this->game->score());
+    }
+
+    public function testTheGameIs20()
+    {
+    }
+
+    private function addSameNumberOfPins($iterarion, $numberOfPins)
+    {
+        for($i = 0; $i < $iterarion; $i++) {
+            $this->game->roll($numberOfPins);
         }
-        $this->assertEquals(0, $game->score());
     }
 }
