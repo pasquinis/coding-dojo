638ce779609e476c363e20b7e28ec5291b8f729b
Simone Pasquini <simone.pasquini@gmail.com>
2016-01-21 13:46:46 +0000

    Introduced the frame logic for browse the roll_history

--- bowling_tdd/101/Game.php
+++ bowling_tdd/101/Game.php
@@ -2,23 +2,31 @@
 
 class Game
 {
-    private $score = 0;
-    private $roll_history = [];
+    private $roll_history;
+    private $rollIndex;
 
     public function __construct()
     {
-        for($rollIndex = 0; $rollIndex < 21; $rollIndex++) {
-            $this->roll_history[$rollIndex] = 0;
+        $this->rollIndex = 0;
+        for($i = 0; $i < 21; $i++) {
+            $this->roll_history[$i] = 0;
         }
     }
 
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->roll_history[$this->rollIndex++] = $pins;
     }
 
     public function score()
     {
-        return $this->score;
+        $score = 0;
+        $i = 0;
+        for($frameIndex = 0; $frameIndex < 10; $frameIndex++) {
+            $score += $this->roll_history[$i] + $this->roll_history[$i + 1];
+            $i += 2;
+        }
+
+        return $score;
     }
 }
