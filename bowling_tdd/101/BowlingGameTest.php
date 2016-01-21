da621eee83e893e4f3644a6e9c7711f569956ff0
Simone Pasquini <simone.pasquini@gmail.com>
2016-01-21 13:01:46 +0000

    Removed all code and commented all tests, starting with the first

--- bowling_tdd/101/BowlingGameTest.php
+++ bowling_tdd/101/BowlingGameTest.php
@@ -15,54 +15,54 @@ class BowlingGameTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(0, $this->game->score());
     }
 
-    public function testTheGameIs20()
-    {
-        $this->addSameNumberOfPins(20, 1);
-        $this->assertEquals(20, $this->game->score());
-    }
+    #public function testTheGameIs20()
+    #{
+    #    $this->addSameNumberOfPins(20, 1);
+    #    $this->assertEquals(20, $this->game->score());
+    #}
 
-    public function testWithOneSpare()
-    {
-        $this->game->roll(7);
-        $this->game->roll(3);
-        $this->game->roll(5);
-        $this->addSameNumberOfPins(17, 0);
-        $this->assertEquals(20, $this->game->score());
-    }
+    #public function testWithOneSpare()
+    #{
+    #    $this->game->roll(7);
+    #    $this->game->roll(3);
+    #    $this->game->roll(5);
+    #    $this->addSameNumberOfPins(17, 0);
+    #    $this->assertEquals(20, $this->game->score());
+    #}
 
-    public function testWithTwoSpare()
-    {
-        $this->game->roll(7);
-        $this->game->roll(3);
-        $this->game->roll(5);
-        $this->game->roll(5);
-        $this->game->roll(3);
-        $this->addSameNumberOfPins(15, 0);
-        $this->assertEquals(31, $this->game->score());
-    }
+    #public function testWithTwoSpare()
+    #{
+    #    $this->game->roll(7);
+    #    $this->game->roll(3);
+    #    $this->game->roll(5);
+    #    $this->game->roll(5);
+    #    $this->game->roll(3);
+    #    $this->addSameNumberOfPins(15, 0);
+    #    $this->assertEquals(31, $this->game->score());
+    #}
 
-    public function testWithOneStrike()
-    {
-        $this->game->roll(10);
-        $this->game->roll(0);
-        $this->game->roll(3);
-        $this->game->roll(3);
-        $this->addSameNumberOfPins(16, 0);
-        $this->assertEquals(22, $this->game->score());
-    }
+    #public function testWithOneStrike()
+    #{
+    #    $this->game->roll(10);
+    #    $this->game->roll(0);
+    #    $this->game->roll(3);
+    #    $this->game->roll(3);
+    #    $this->addSameNumberOfPins(16, 0);
+    #    $this->assertEquals(22, $this->game->score());
+    #}
 
-    public function testPerfectGame()
-    {
-        $this->addFullSequenceOfStrike();
-        $this->assertEquals(300, $this->game->score());
-    }
+    #public function testPerfectGame()
+    #{
+    #    $this->addFullSequenceOfStrike();
+    #    $this->assertEquals(300, $this->game->score());
+    #}
 
-    private function addFullSequenceOfStrike()
-    {
-        for($i = 1; $i <= 12; $i++) {
-            $this->game->roll(10);
-        }
-    }
+    #private function addFullSequenceOfStrike()
+    #{
+    #    for($i = 1; $i <= 12; $i++) {
+    #        $this->game->roll(10);
+    #    }
+    #}
 
     private function addSameNumberOfPins($iterarion, $numberOfPins)
     {
--- bowling_tdd/101/Game.php
+++ bowling_tdd/101/Game.php
@@ -2,77 +2,11 @@
 
 class Game
 {
-
-    private $array_of_rolls = [];
-
     public function roll($pins)
     {
-        $this->array_of_rolls[] = $pins;
     }
 
     public function score()
     {
-        $total = 0;
-        for($i = 0; $i < 20; $i = $i + 2) {
-            $bonus = 0;
-            $actual = $this->actualRoll($i);
-            $next = $this->nextRoll($i);
-            #echo "identifier: $i actual: $actual, next: $next" . PHP_EOL;
-            if ($this->isStrike($actual)) {
-                $bonus = $this->strikeBonus($i);
-                #echo "bonus $bonus" .PHP_EOL;
-            } elseif ($this->isSpare($actual, $next)) {
-                $bonus = $this->spareBonus($i);
-            }
-            $total += $actual + $next + $bonus;
-            #echo "total: $total" . PHP_EOL;
-        }
-        return $total;
-    }
-
-    private function isStrike($actual)
-    {
-        return $actual == 10;
-    }
-
-    private function isSpare($actual, $next)
-    {
-        return ($actual + $next) == 10;
-    }
-
-    private function strikeBonus($rollIdentifier)
-    {
-       return $this->firstRollOfNextFrame($rollIdentifier) + $this->secondRollOfNextFrame($rollIdentifier);
-    }
-
-    private function spareBonus($rollIdentifier)
-    {
-        #echo "the bonus is " . $this->firstRollOfNextFrame($rollIdentifier) . " for roll:" . $rollIdentifier . PHP_EOL;
-        return $this->firstRollOfNextFrame($rollIdentifier);
-    }
-
-    private function firstRollOfNextFrame($rollIdentifier)
-    {
-        return isSet($this->array_of_rolls[$rollIdentifier + 2]) ? $this->array_of_rolls[$rollIdentifier + 2] : 0;
-    }
-
-    private function secondRollOfNextFrame($rollIdentifier)
-    {
-        return isSet($this->array_of_rolls[$rollIdentifier + 3]) ? $this->array_of_rolls[$rollIdentifier + 3]: 0;
-    }
-
-    private function actualRoll($rollIdentifier)
-    {
-        return $this->array_of_rolls[$rollIdentifier];
-    }
-
-    private function nextRoll($rollIdentifier)
-    {
-        return $this->array_of_rolls[$rollIdentifier + 1];
-    }
-
-    private function isNextRollPresent($rollIdentifier)
-    {
-        return isSet($this->array_of_rolls[$rollIdentifier + 1]);
     }
 }
