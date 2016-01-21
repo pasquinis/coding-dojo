a87d6f6fed445f349d5963c29d2bfb5f2667c55c
Simone Pasquini <simone.pasquini@gmail.com>
2016-01-21 15:58:05 +0000

    Added testAllPinsIsTwo and the naive implentation is ok

--- bowling_ocp/101/BowlingGameTest.php
+++ bowling_ocp/101/BowlingGameTest.php
@@ -16,6 +16,12 @@ class BowlingGameTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(0, $this->game->score());
     }
 
+    public function testAllPinsIsTwo()
+    {
+        $this->multipleRollWith(20, 2);
+        $this->assertEquals(40, $this->game->score());
+    }
+
     private function multipleRollWith($iteration, $pins)
     {
         for($i=0; $i < $iteration; $i++) {
