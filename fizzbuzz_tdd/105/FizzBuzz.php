fbf38137b7d835c872aa9fd318a8182843e98b2c
Simone Pasquini <simone.pasquini@gmail.com>
2016-02-23 08:01:15 +0000

    Removed the if duplication with array iteration

--- fizzbuzz_tdd/105/FizzBuzz.php
+++ fizzbuzz_tdd/105/FizzBuzz.php
@@ -9,19 +9,20 @@ class FizzBuzz
     public function __construct($number)
     {
         $this->number = $number;
-        $this->fizz = new Fizz();
-        $this->buzz = new Buzz();
+        $this->rules = [
+            new Fizz(),
+            new Buzz()
+        ];
     }
 
     public function say()
     {
         $message = '';
-        if ($this->fizz->isMultipleOf($this->number)) {
-            $message .= $this->fizz->say();
-        }
 
-        if ($this->buzz->isMultipleOf($this->number)) {
-            $message .= $this->buzz->say();
+        foreach($this->rules as $rule) {
+            if ($rule->isMultipleOf($this->number)) {
+                $message .= $rule->say();
+            }
         }
 
         if (!empty($message))
