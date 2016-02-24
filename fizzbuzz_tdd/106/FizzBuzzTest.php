881156f1077102b5d1f638eddc7e4e9cee864a16
Simone Pasquini <simone.pasquini@gmail.com>
2016-02-24 07:57:25 +0000

    Test when I say two

--- fizzbuzz_tdd/106/FizzBuzzTest.php
+++ fizzbuzz_tdd/106/FizzBuzzTest.php
@@ -9,4 +9,10 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
         $fizz = new FizzBuzz();
         $this->assertEquals(1, $fizz->say(1));
     }
+
+    public function testWhenISay2TheResponseIs2()
+    {
+        $fizz = new FizzBuzz();
+        $this->assertEquals(2, $fizz->say(2));
+    }
 }
