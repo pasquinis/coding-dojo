35de62a272fcf79bfbd13797d735dc22ba0b0653
Simone Pasquini <simone.pasquini@gmail.com>
2016-02-24 08:00:39 +0000

    Added Fizz test when number is 3

--- fizzbuzz_tdd/106/FizzBuzzTest.php
+++ fizzbuzz_tdd/106/FizzBuzzTest.php
@@ -15,4 +15,10 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
         $fizz = new FizzBuzz();
         $this->assertEquals(2, $fizz->say(2));
     }
+
+    public function testWhenISay3TheResponseIsFizz()
+    {
+        $fizz = new FizzBuzz();
+        $this->assertEquals('Fizz', $fizz->say(3));
+    }
 }
