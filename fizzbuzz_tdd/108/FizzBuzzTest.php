1a50f5cc0bcda75cf5ea06fc02cf8a7cc930c7f4
Simone Pasquini <simone.pasquini@gmail.com>
2016-02-26 07:43:33 +0000

    Testing Fizz when a number is multiple of three

--- fizzbuzz_tdd/108/FizzBuzzTest.php
+++ fizzbuzz_tdd/108/FizzBuzzTest.php
@@ -9,10 +9,22 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
         $fizz = new FizzBuzz();
         $this->assertEquals(1, $fizz->say(1));
     }
-    
+
     public function testGivenANumber2ThenTheResponseIs2()
     {
         $fizz = new FizzBuzz();
         $this->assertEquals(2, $fizz->say(2));
+    }
+
+    public function testGivenANumber3ThenTheResponseIsFizz()
+    {
+        $fizz = new FizzBuzz();
+        $this->assertEquals('Fizz', $fizz->say(3));
+    }
+
+    public function testGivenANumberMultiplerOf3ThenTheResponseIsFizz()
+    {
+        $fizz = new FizzBuzz();
+        $this->assertEquals('Fizz', $fizz->say(3*2));
     }
 }
