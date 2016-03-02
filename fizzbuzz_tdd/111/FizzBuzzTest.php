5fdc777e1714e461f30749940cc0e900d6049229
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-02 07:48:47 +0000

    Added test when number is three the word is Fizz

--- fizzbuzz_tdd/111/FizzBuzzTest.php
+++ fizzbuzz_tdd/111/FizzBuzzTest.php
@@ -4,15 +4,21 @@ require_once 'FizzBuzz.php';
 
 class FizzBuzzTest extends \PHPUnit_Framework_TestCase
 {
-    public function testWhenISayNumberOneThenIReceiveOneWord()
+    public function testWhenISayNumberOneThenIReceiveWordOne()
     {
         $fizz = new FizzBuzz();
         $this->assertEquals(1, $fizz->say(1));
     }
 
-    public function testWhenISayNumberTwoThenIReceiveOneTwo()
+    public function testWhenISayNumberTwoThenIReceiveWordTwo()
     {
         $fizz = new FizzBuzz();
         $this->assertEquals(2, $fizz->say(2));
+    }
+
+    public function testWhenISayNumberThreeThenIReceiveWordFizz()
+    {
+        $fizz = new FizzBuzz();
+        $this->assertEquals('Fizz', $fizz->say(3));
     }
 }
