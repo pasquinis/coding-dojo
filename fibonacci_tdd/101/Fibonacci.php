9138e9476157921de186db553d1c7cd1fca2437a
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-03 11:16:51 +0000

    Refactoring: removed orphan return

--- fibonacci_tdd/101/Fibonacci.php
+++ fibonacci_tdd/101/Fibonacci.php
@@ -7,10 +7,7 @@ class Fibonacci
         if ($aNumber == 0) return 0;
         if ($aNumber == 1) return 1;
         if ($aNumber >= 2) {
-            $output = $this->calculate($aNumber - 1) + $this->calculate($aNumber - 2);
-            return $output;
+            return $this->calculate($aNumber - 1) + $this->calculate($aNumber - 2);
         }
-
-        return 1;
     }
 }
