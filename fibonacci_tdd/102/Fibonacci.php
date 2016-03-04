89ced1732dbd7913d48d4f3fdb9deeb5a485c3fc
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-04 08:07:24 +0000

    Adding test when input number is three

--- fibonacci_tdd/102/Fibonacci.php
+++ fibonacci_tdd/102/Fibonacci.php
@@ -4,10 +4,11 @@ class Fibonacci
 {
     public function compute($aNumber)
     {
-        if ($aNumber >= 1) {
+        if ($aNumber == 0) return 0;
+        if ($aNumber > 2) {
+            return 2;
+        } else {
             return 1;
         }
-
-        return 0;
     }
 }
