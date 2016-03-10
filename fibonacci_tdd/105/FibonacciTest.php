bbbe97a57e4aae432b3247049886b2ce93da51f1
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-10 07:56:48 +0000

    Added test when input number is 1

--- fibonacci_tdd/105/FibonacciTest.php
+++ fibonacci_tdd/105/FibonacciTest.php
@@ -4,9 +4,15 @@ require_once 'Fibonacci.php';
 
 class FibonacciTest extends \PHPUnit_Framework_TestCase
 {
-    public function testWhenIEnterNumberZeroThenIExpectNumberoZero()
+    public function testWhenIEnterNumberZeroThenIExpectNumberZero()
     {
         $fibonacci = new Fibonacci();
         $this->assertEquals(0, $fibonacci->compute(0));
+    }
+
+    public function testWhenIEnterNumberOneThenIExpectNumberOne()
+    {
+        $fibonacci = new Fibonacci();
+        $this->assertEquals(1, $fibonacci->compute(1));
     }
 }
