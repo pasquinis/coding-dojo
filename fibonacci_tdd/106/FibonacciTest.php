4ebef887b662eeb8021019a93e098a98eb079ae9
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-11 08:03:37 +0000

    Added fifth test, when the number is four

--- fibonacci_tdd/106/FibonacciTest.php
+++ fibonacci_tdd/106/FibonacciTest.php
@@ -27,4 +27,10 @@ class FibonacciTest extends \PHPUnit_Framework_TestCase
         $fibonacci = new Fibonacci();
         $this->assertEquals(2, $fibonacci->compute(3));
     }
+
+    public function testGivenTheNumberFourThenTheComputationIsThree()
+    {
+        $fibonacci = new Fibonacci();
+        $this->assertEquals(3, $fibonacci->compute(4));
+    }
 }
