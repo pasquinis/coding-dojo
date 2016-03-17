2cb34eacc58d81d5ae300f4eff52b66efc4709c1
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-17 07:45:39 +0000

    Started with test when I input number zero

---
+++ fibonacci_tdd/110/FibonacciTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'Fibonacci.php';
+
+class FibonacciTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWhenIInputNumberZeroThenIReceiveNumberZero()
+    {
+        $fibonacci = new Fibonacci();
+        $this->assertEquals(0, $fibonacci->compute(0));
+    }
+}
