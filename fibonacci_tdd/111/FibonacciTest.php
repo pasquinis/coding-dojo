e84ebcb7cf79369cc1cc72f94c0b4cfe232d1a72
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-18 08:12:12 +0000

    Refactoring: introduced setUp

--- fibonacci_tdd/111/FibonacciTest.php
+++ fibonacci_tdd/111/FibonacciTest.php
@@ -5,15 +5,18 @@ require_once 'Fibonacci.php';
 class FibonacciTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected function setUp()
+    {
+        $this->fibonacci = new Fibonacci();
+    }
+
     public function testWhenIInputNumberZeroThenIComputeZero()
     {
-        $fibonacci = new Fibonacci();
-        $this->assertEquals(0, $fibonacci->compute(0));
+        $this->assertEquals(0, $this->fibonacci->compute(0));
     }
 
     public function testWhenIInputNumberOneThenIComputeOne()
     {
-        $fibonacci = new Fibonacci();
-        $this->assertEquals(1, $fibonacci->compute(1));
+        $this->assertEquals(1, $this->fibonacci->compute(1));
     }
 }
