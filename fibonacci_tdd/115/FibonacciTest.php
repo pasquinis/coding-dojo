06338df1b9a23423d3d2420594599ad76bfd9d8a
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-24 07:45:35 +0000

    Added test when I input number one

--- fibonacci_tdd/115/FibonacciTest.php
+++ fibonacci_tdd/115/FibonacciTest.php
@@ -4,9 +4,19 @@ require_once 'Fibonacci.php';
 
 class FibonacciTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected function setUp()
+    {
+        $this->fibonacci = new Fibonacci();
+    }
+
     public function testShouldCalculateWhenIInputNumberZero()
     {
-        $fibonacci = new Fibonacci();
-        $this->assertEquals(0, $fibonacci->compute(0));
+        $this->assertEquals(0, $this->fibonacci->compute(0));
+    }
+
+    public function testShouldCalculateOneWhenIInputNumberOne()
+    {
+        $this->assertEquals(1, $this->fibonacci->compute(1));
     }
 }
