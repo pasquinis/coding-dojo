6bf69750cb710cb51908366366ab50d805a4bbbc
Simone Pasquini <simone.pasquini@gmail.com>
2016-03-25 13:01:31 +0000

    Added mapping function
    Where I compare if the number passed is equal then number one

--- bank_ocr/101/BankOcr.php
+++ bank_ocr/101/BankOcr.php
@@ -5,6 +5,15 @@ class BankOcr
     public function translate($aNumber)
     {
         if ($aNumber == '') return "";
-        return "1";
+        return $this->mapping($aNumber);
     } 
+
+    private function mapping($aNumber)
+    {
+        $one = <<<EOF
+  |
+  |
+EOF;
+        if ($one == $aNumber) return "1";
+    }
 }
