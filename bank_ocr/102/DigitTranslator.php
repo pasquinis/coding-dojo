774099a10d9aab003bde5fdae84c4ef856799bc3
Simone Pasquini <simone.pasquini@gmail.com>
2016-04-04 07:24:15 +0000

    Added test for FIVE implementation

--- bank_ocr/102/DigitTranslator.php
+++ bank_ocr/102/DigitTranslator.php
@@ -23,11 +23,17 @@ class DigitTranslator
         "|_|" .
         "  |";
 
+    const FIVE =
+        " _ " .
+        "|_ " .
+        " _|";
+
     public function translateDigitToNumber($aDigit)
     {
         if ($aDigit == self::ONE) return 1;
         if ($aDigit == self::TWO) return 2;
         if ($aDigit == self::THREE) return 3;
         if ($aDigit == self::FOUR) return 4;
+        if ($aDigit == self::FIVE) return 5;
     }
 }
