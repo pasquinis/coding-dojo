f8ce27c696c3bc999e569c187ef4f3840dbc9706
Simone Pasquini <simone.pasquini@gmail.com>
2016-04-21 06:44:40 +0000

    The game start with a Glider

--- game_of_life/101/intercativeGol.php
+++ game_of_life/101/intercativeGol.php
@@ -3,9 +3,7 @@
 require_once 'GameOfLife.php';
 
 $gol = new GameOfLife(5, 10);
-$gol->add([[1, 1] , [1, 2], [1, 3]]);
-$gol->add([[3, 1] , [3, 2], [3, 3]]);
-$gol->add([[2, 8] , [2, 9], [3, 8], [3, 9]]);
+$gol->add([[1, 2], [2, 3], [3, 1], [3, 2], [3, 3] ]);
 echo $gol->grid();
 
 while(true) {
