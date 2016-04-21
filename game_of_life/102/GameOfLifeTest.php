6b3bdcdd7d54628962b43eb1d33bcfcb7ee40b5e
Simone Pasquini <simone.pasquini@gmail.com>
2016-04-21 06:46:02 +0000

    Added assertion, the cell dies also when have zero neighbours

--- game_of_life/102/GameOfLifeTest.php
+++ game_of_life/102/GameOfLifeTest.php
@@ -4,9 +4,13 @@ require_once 'GameOfLife.php';
 
 class GameOfLifeTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGivenALiveCellWithOneNeighboursTheCellDies()
+    public function testGivenALiveCellWithFewerThanTwoNeighboursTheCellDies()
     {
         $gol = new GameOfLife();
+        $this->assertEquals(
+            GameOfLife::DEAD,
+            $gol->nextGeneration(GameOfLife::ALIVE, $neighbours = 0)
+        );
         $this->assertEquals(
             GameOfLife::DEAD,
             $gol->nextGeneration(GameOfLife::ALIVE, $neighbours = 1)
