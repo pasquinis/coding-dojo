1fc63a46d04d9d245a1ab0084fd6182918cd58d1
Simone Pasquini <simone.pasquini@gmail.com>
2016-05-03 06:49:31 +0000

    Added final test
    With an array of ten elements

--- sort/selection_sort/102/SelectionSortTest.php
+++ sort/selection_sort/102/SelectionSortTest.php
@@ -19,5 +19,9 @@ class SelectionSortTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals([1, 2], $this->selection->sort([2, 1]));
         $this->assertEquals([1, 2, 3], $this->selection->sort([2, 3, 1]));
+        $this->assertEquals(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            $this->selection->sort([2, 3, 6, 5, 8, 7, 10, 9, 4, 1])
+        );
     }
 }
