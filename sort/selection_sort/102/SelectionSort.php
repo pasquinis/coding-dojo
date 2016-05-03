1be91878b6f5c3d6478de8d2805b4c934e85304f
Simone Pasquini <simone.pasquini@gmail.com>
2016-05-03 07:05:01 +0000

    Selection sort with only swap the index

--- sort/selection_sort/102/SelectionSort.php
+++ sort/selection_sort/102/SelectionSort.php
@@ -3,14 +3,16 @@
 class SelectionSort
 {
     public function sort(array $unsorted)
-    {
-       for ($j = 0; $j < count($unsorted); $j++) {
+    { 
+        for ($j = 0; $j < count($unsorted); $j++) {
+            $iMin = $j;
             for ($i = 1 + $j; $i < count($unsorted); $i++) {
-                if ($unsorted[$i] < $unsorted[$j]) {
-                    $temp = $unsorted[$j];
-                    $unsorted[$j] = $unsorted[$i];
-                    $unsorted[$i] = $temp;
+                if ($unsorted[$i] < $unsorted[$iMin]) {
+                    $iMin = $i;
                 }
+                $temp = $unsorted[$iMin];
+                $unsorted[$iMin] = $unsorted[$j];
+                $unsorted[$j] = $temp;
             }
         }
 
