08db5e435ef201aa401c3f0fefd9bded2626ae39
Simone Pasquini <simone.pasquini@gmail.com>
2016-05-05 06:35:18 +0000

    Sorting an unsorted array with two elements

--- sort/selection_sort/103/SelectionSort.php
+++ sort/selection_sort/103/SelectionSort.php
@@ -2,8 +2,15 @@
 
 class SelectionSort
 {
-    public function sort()
+    public function sort($unsorted)
     {
-        return [1];
+        for ($i = 1; $i < count($unsorted); $i++) {
+            if ($unsorted[$i] < $unsorted[0]) {
+                $temp = $unsorted[0];
+                $unsorted[0] = $unsorted[$i];
+                $unsorted[$i] = $temp;
+            }
+        }
+        return $unsorted;
     }
 }
--- sort/selection_sort/103/SelectionSortTest.php
+++ sort/selection_sort/103/SelectionSortTest.php
@@ -4,9 +4,19 @@ require 'SelectionSort.php';
 
 class SelectionSortTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected function setUp()
+    {
+        $this->selection = new SelectionSort();
+    }
+
     public function testShouldOrderAnArrayOfOneElement()
     {
-        $selection = new SelectionSort();
-        $this->assertEquals([1], $selection->sort([1]));
+        $this->assertEquals([1], $this->selection->sort([1]));
+    }
+
+    public function testShouldSortAnArrayUnorderedWithTwoElements()
+    {
+        $this->assertEquals([1, 2], $this->selection->sort([2, 1]));
     }
 }
