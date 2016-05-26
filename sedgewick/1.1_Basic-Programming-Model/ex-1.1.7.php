72de1176b2d8098ab215f33083484b2b6e96a5c3
Simone Pasquini <simone.pasquini@gmail.com>
2016-05-26 06:39:40 +0000

    Closed the exercise
    The question number c return 0 because the variable N is not initialized

--- sedgewick/1.1_Basic-Programming-Model/ex-1.1.7.php
+++ sedgewick/1.1_Basic-Programming-Model/ex-1.1.7.php
@@ -4,11 +4,29 @@ $t = 9.0;
 
 while (abs($t - 9.0/$t) > .001) {
     $t = (9.0 / $t + $t) / 2.0;
-    var_dump($t);
 }
 
-echo sprintf("%.5f\n", $t);
-
+echo sprintf("%.5f\n", $t) . PHP_EOL;
 
 /*** ***/
 
+$sum = 0;
+for ($i = 1; $i < 1000; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        $sum++;
+    }
+}
+
+echo $sum . PHP_EOL;
+
+/*** ***/
+
+$sum = 0;
+
+for ($i = 1; $i < 1000; $i *= 2) {
+    for ($j = 0; $j < N; $j++) {
+        $sum++;
+    }
+}
+
+echo $sum . PHP_EOL;
