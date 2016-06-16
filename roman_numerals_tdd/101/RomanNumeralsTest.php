ff891677d98b6ecf0dbb25f63a65d880afb14f78
Simone Pasquini <simone.pasquini@gmail.com>
2016-06-16 07:08:36 +0000

    Starting the skel of project

---
+++ roman_numerals_tdd/101/RomanNumeralsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testICanCovertNumberOneToRomanNumber()
+    {
+        $this->assertFalse(true);
+    }
+}
