74b76bd68815cceabf4f85972a1ac3566b294190
Simone Pasquini <simone.pasquini@gmail.com>
2016-11-11 11:29:34 +0100

    Refactored with rules array

--- fizzbuzz_tdd/202/FizzBuzz.php
+++ fizzbuzz_tdd/202/FizzBuzz.php
@@ -2,16 +2,23 @@
 
 class FizzBuzz
 {
+    const EMPTY_WORD = '';
+
+    public function __construct()
+    {
+        $this->rules = [3 => 'Fizz', 5 => 'Buzz'];
+    }
+
     public function say($number)
     {
-        if ($number % 3 == 0) {
-            return 'Fizz';
+        $response = '';
+        foreach ($this->rules as $divisor => $word) {
+            $response .= ($number % $divisor == 0) ? $word : self::EMPTY_WORD;
         }
 
-        if ($number % 5 == 0) {
-            return 'Buzz';
+        if (!empty($response)) {
+            return $response;
         }
-
         return $number;
     }
 }
